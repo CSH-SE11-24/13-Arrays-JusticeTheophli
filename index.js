c619cc921647198e274ff06691a0cb8e77c7db38
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 11:32:40 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// Create an empty array to store 3 favorite snacks.
+
+
+// Console log the 2nd element of the array
+
+
+// Add a new snack to the end of the array.
+
+
+// Console log the last element of the array using array.length to check
+
+
+// Add another snack to the start of the array.
+
+
+// Console log the length of the array (should be 5!)
+
+
+// Change the 3rd element to a different snack
+
+
+// Remove the first snack from the array
+
+
+// Remove the last snack from the array
+
+
+// Console log the whole array to check if your operations worked
+
+
